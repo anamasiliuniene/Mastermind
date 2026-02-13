d3c345154d0909d2faffca5d0d60781b6c87201a
anamasiliuniene <ana.masiliuniene@gmail.com>
2026-02-13 14:03:56 +0200

    admin puslapis pritaikytas, laiko zona

--- mysite/mastermind/admin.py
+++ mysite/mastermind/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Post, Comment
 
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created')
+    inlines = [CommentInline]
+    readonly_fields = ('created',)
+    list_filter = ('author','created',)
+    search_fields = ('title', 'content', 'author__username')
+
+
+    fieldsets = [
+        ('General', {'fields': ('title', 'content', 'author', 'created')})
+    ]
+
+
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
+admin.site.register(Post, PostAdmin)
