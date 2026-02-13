a9df397d9f29ebf1e0e0b9349dc79474c3cf253e
anamasiliuniene <ana.masiliuniene@gmail.com>
2026-02-13 13:41:18 +0200

    aprasyti modeliai, registruoti admin.py

--- mysite/mastermind/models.py
+++ mysite/mastermind/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+# User, Goal, GoalTask, Assignment, UserAssignment, Comment
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(to=User,
+                               on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(to="Post",
+                             on_delete=models.CASCADE,
+                             related_name="comments")
+    content = models.TextField()
+    author = models.ForeignKey(to=User,
+                               on_delete=models.SET_NULL,
+                               null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.content}"
+
